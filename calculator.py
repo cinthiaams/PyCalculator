846acbd3e4936348398ea25f2abd7b890993a083
cinthiaams <cinthia_ams@hotmail.com>
2024-07-02 10:51:49 +0100

    feat: adding an Welcome frase and a better error handling

--- calculator.py
+++ calculator.py
@@ -5,38 +5,45 @@
 # for this we will use some variales, the input function and we are gonna to convert the 
 # insert numbers from string to float
 
-valid_operator = ['+', '-', '*', '/', '%']
-operation = input('''To get started: 
-please type in the math operation you would like to complete:
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
-% for percentage
-''')
-# in case of erro
-if operation not in valid_operator:
-    print('You have not type a valid operator, please run the program again.')
-else:   
-    nmb1 = float(input('Enter your first number: '))
-    nmb2 = float(input('Enter your second number: '))
+#Welcome
+print("Welcome to PyCalculator!")
+# Loop
+while True:
+    # variable that stores valid operators
+    valid_operator = ['+', '-', '*', '/', '%']
+    operation = input('''Enter the calculation operator you would like to use:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    % for percentage
+    ''')
+    # in case of erro
+    if operation not in valid_operator:
+        print('You have not type a valid operator, please try again.\n')
+        continue
+    # 
+    else:   
+        nmb1 = float(input('Enter your first number: '))
+        nmb2 = float(input('Enter your second number: '))
 
-    # Addition
-    if operation == '+':
-        print('{} + {} = {}'.format(nmb1, nmb2, nmb1 + nmb2))
+        # Addition
+        if operation == '+':
+            print('{} + {} = {}'.format(nmb1, nmb2, nmb1 + nmb2))
 
-    # Subtraction
-    elif operation == '-':
-        print('{} - {} = {}'.format(nmb1, nmb2, nmb1 - nmb2))
+        # Subtraction
+        elif operation == '-':
+            print('{} - {} = {}'.format(nmb1, nmb2, nmb1 - nmb2))
 
-    # Multiplication
-    elif operation == '*':
-        print('{} x {} = {}'.format(nmb1, nmb2, nmb1 * nmb2))
+        # Multiplication
+        elif operation == '*':
+            print('{} x {} = {}'.format(nmb1, nmb2, nmb1 * nmb2))
 
-    # Division
-    elif operation == '/':
-        print('{} / {} = {}'.format(nmb1, nmb2, nmb1 / nmb2))
+        # Division
+        elif operation == '/':
+            print('{} / {} = {}'.format(nmb1, nmb2, nmb1 / nmb2))
 
-    # Percentage
-    elif operation == '%':
-        print('{} % {} = {}'.format(nmb1, nmb2, nmb1 * (nmb2 / 100)))
+        # Percentage
+        elif operation == '%':
+            print('{} % {} = {}'.format(nmb1, nmb2, nmb1 * (nmb2 / 100)))
+        break
