71df319b0c4f63d68372d12e746988b122a56c0f
cinthiaams <cinthia_ams@hotmail.com>
2024-07-02 14:41:35 +0100

    test: refactoring

--- calculator.py
+++ calculator.py
@@ -1,14 +1,15 @@
 # Starting the first PYTHON PRACTICE PROJECT starring Cinthia Morais
 # Today's project is a... CALCULATOR!
 
+# Function for inputting numbers
 def number_input(prompt):
     while True:
         valor = input(prompt)
         try:
-        #converter para float
+        # Convert to float
             return float(valor)
         except ValueError:
-        #se houver uma virgula substitui para ponto e tenta de novo
+        # If there is a comma, replace it with a period and try again
             if ',' in valor:
                 try:
                     return float(valor.replace(',','.'))
@@ -16,6 +17,7 @@ def number_input(prompt):
                     print('Invalid Value. Try Again\n')
             else:
                 print('Invalid Value. Try Again\n')
+# Function for inputting operator
 def operator_input():
     valid_operator = ['+', '-', '*', '/', '%']
     while True:  
@@ -30,9 +32,9 @@ print('+--------------------------+')
 print('| Welcome to PyCalculator! |')
 print('+--------------------------+\n')
 
+# PyCalculator main function
 def pycalculator():        
     
-     
     nmb1 = number_input('Enter your first number: ')
     operation = operator_input()
     nmb2 = number_input('\nEnter your second number: ')
@@ -54,13 +56,13 @@ def pycalculator():
         if nmb2 != 0:
             print('{} / {} = {}'.format(nmb1, nmb2, nmb1 / nmb2))
         else:
-            print("Não é possivel dividir por zero")
+            print("It is not possible to divide by zero")
 
     # Percentage
     elif operation == '%':
         print('{} % {} = {}'.format(nmb1, nmb2, nmb1 * (nmb2 / 100)))
     again()
-
+# Function to know if the user would like to calculate again
 def again():
     calc_again = input('''
 Do you want to calculate again?
