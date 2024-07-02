5e603c0a8bdfab115142f9abcde59c17d378807c
cinthiaams <cinthia_ams@hotmail.com>
2024-07-02 10:10:33 +0100

    new feature: adding percentage and error handling

--- calculator.py
+++ calculator.py
@@ -5,19 +5,38 @@
 # for this we will use some variales, the input function and we are gonna to convert the 
 # insert numbers from string to float
 
-input('To get started, please press enter.')
+valid_operator = ['+', '-', '*', '/', '%']
+operation = input('''To get started: 
+please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+% for percentage
+''')
+# in case of erro
+if operation not in valid_operator:
+    print('You have not type a valid operator, please run the program again.')
+else:   
+    nmb1 = float(input('Enter your first number: '))
+    nmb2 = float(input('Enter your second number: '))
 
-nmb1 = float(input('Enter your first number: '))
-nmb2 = float(input('Enter your second number: '))
+    # Addition
+    if operation == '+':
+        print('{} + {} = {}'.format(nmb1, nmb2, nmb1 + nmb2))
 
-# Addition
-print('{} + {} = {}'.format(nmb1, nmb2, nmb1 + nmb2))
+    # Subtraction
+    elif operation == '-':
+        print('{} - {} = {}'.format(nmb1, nmb2, nmb1 - nmb2))
 
-# Subtraction
-print('{} - {} = {}'.format(nmb1, nmb2, nmb1 - nmb2))
+    # Multiplication
+    elif operation == '*':
+        print('{} x {} = {}'.format(nmb1, nmb2, nmb1 * nmb2))
 
-# Multiplication
-print('{} x {} = {}'.format(nmb1, nmb2, nmb1 * nmb2))
+    # Division
+    elif operation == '/':
+        print('{} / {} = {}'.format(nmb1, nmb2, nmb1 / nmb2))
 
-# Division
-print('{} / {} = {}'.format(nmb1, nmb2, nmb1 / nmb2))
+    # Percentage
+    elif operation == '%':
+        print('{} % {} = {}'.format(nmb1, nmb2, nmb1 * (nmb2 / 100)))
